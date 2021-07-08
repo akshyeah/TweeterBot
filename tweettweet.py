265a3c560277c6003b3b68564e21b090ea2d28e2
Akshay kumar <akshay1.kumar@lntinfotech.com>
2021-07-08 16:52:09 +0530

    string search

--- tweettweet.py
+++ tweettweet.py
@@ -3,8 +3,8 @@ import time
 
 consumer_key = 'YSiiIRTe0poG7CNyXBQeRu96w'
 consumer_secret = '5S9hHgQsCDwrePKPj8KyaZObQsO84O3VWyYjhaeNSSTVXJFZyL'
-access_token = '862738332-lCGcx3y0zfneT91jrjpQJ8XuIBHRvSNE0dhqgty8'
-access_token_secret = 'RpqSvQTdiQcpvAIGACHRUdNhrjApWCs7iJxahJBUbmErL'
+access_token = '862738332-6nkB4hcFJDKjUbHV3LDk4mrEY4EJfbPujDy5zQmk'
+access_token_secret = 'fttyFJ3P4UlBQ27i0LAREkDLitBot4jbdIUHfuHJDXNn5'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -20,8 +20,25 @@ def limit_handler(cursor):
         while True:
             yield cursor.next()
     except tweepy.RateLimitError:
-        time.sleep(1000)
+        time.sleep(300)
 
-#Generous bot
-for follower in limit_handler(tweepy.Cursor(api.followers).items()):
-    print(follower.name)
+
+
+search_string = 'python'
+numberOfTweets = 2
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+# #Generous bot
+# for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+#     if follower.name == 'Shivani':
+#         follower.follow()
+#         break
